9b1e492ac40ab61e683bc125c037051fe7adad64
KhanhHG <honggiakhanh@gmail.com>
2023-12-07 19:26:12 +0200

    add day4-2

--- day4-2.js
+++ day4-2.js
@@ -225,10 +225,41 @@ Card 223: 76 93 82 50 99 57 92  1 54  3 |  2  4 27  7 98 95 77 65 41 91 97 73 67
     "\n"
   );
 
-input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`.split("\n");
+// input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`.split("\n");
 
+let count = 0;
+
+let cardCount = [];
+input.map(() => cardCount.push(1));
+
+let cardArr = input
+  .map((row) => row.substring(row.indexOf(":") + 2))
+  .map((row, cardnum) => {
+    let winningArr = row
+      .slice(0, row.indexOf("|"))
+      .match(/\d+/g)
+      .map((num) => num.trim());
+    let yourArr = row
+      .slice(row.indexOf("|"), row.length)
+      .match(/\d+/g)
+      .map((num) => num.trim());
+
+    let winCount = 0;
+
+    yourArr.filter((num) => {
+      if (winningArr.includes(num)) {
+        winCount++;
+      }
+    });
+
+    for (count = 1; count < winCount + 1; count++) {
+      cardCount[cardnum + count] += cardCount[cardnum];
+    }
+  });
+
+console.log(cardCount.reduce((a, b) => a + b));
